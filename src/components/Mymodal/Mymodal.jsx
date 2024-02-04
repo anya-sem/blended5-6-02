ce15afcd18882886fbcf2fc9a40c1c0671cd176d
IrinaLender27 <irina.lender27@gmail.com>
2024-02-04 13:18:08 +0200

    update

---
+++ src/components/Mymodal/Mymodal.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+export const Mymodal = ({ src, alt, modalIsOpen, closeModal }) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <img src={src.large} alt={alt} />
+    </Modal>
+  );
+};
