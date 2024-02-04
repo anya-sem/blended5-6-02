cd0d66251bdaa175dbf50b86b3fafc72fa0a802f
anya-sem <139631738+anya-sem@users.noreply.github.com>
2024-02-04 11:31:33 +0200

    1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,38 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css'
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+
+
+export const Form = ({ onSubmit }) => {
+  const [query, setQuery] = useState('')
+
+  const handleChange = ({ target: {value}}) => {
+    setQuery(value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!query.trim()) {
+      return 
+    }
+    onSubmit(query)
+    setQuery('')
+  }
+  
+  return <form className={style.form} onSubmit={handleSubmit}>
+  <button className={style.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <input
+    className={style.input}
+    placeholder="What do you want to write?"
+    name="search"
+    required
+      autoFocus
+      value={query}
+      onChange={handleChange}
+  />
+</form>;
 };
